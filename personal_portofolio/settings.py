4088be00d62b2a8cb20fbececa0352d41c32d5cc
unknown <mircea23.apopei@gmail.com>
2022-01-17 12:51:21 +0200

    local_settings added

--- personal_portofolio/settings.py
+++ personal_portofolio/settings.py
@@ -131,3 +131,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 
+try:
+    from .local_settings import *
+except ImportError:
+    print("Looks like no local file. You must be on production")
